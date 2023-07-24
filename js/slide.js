566cdcd456bfffc14b786db90151008a12bce64c
yugo-0722 <yugo07222010@gmail.com>
2023-07-24 17:43:08 +0900

    setup

---
+++ js/slide.js
@@ -0,0 +1,39 @@
+$('.slider').slick({
+    autoplay: true,//自動的に動き出すか。初期値はfalse。
+    infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+    slidesToShow: 3,//スライドを画面に3枚見せる
+    slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+    prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+    nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+    dots: false,//下部ドットナビゲーションの非表示
+    pauseOnFocus: false,//フォーカスで一時停止
+    pauseOnHover: false,//マウスホバーで一時停止
+    pauseOnDotsHover: false,//ドットナビをマウスホバーで一時停止
+    responsive: [
+        {
+        breakpoint: 769,//モニターの横幅が769px以下の見せ方
+        settings: {
+            slidesToShow: 2,//スライドを画面に2枚見せる
+            slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+        }
+    },
+    {
+        breakpoint: 426,//モニターの横幅が426px以下の見せ方
+        settings: {
+            slidesToShow: 1,//スライドを画面に1枚見せる
+            slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+        }
+    }
+]
+// responsive: [
+//     {
+//       breakpoint: 450, // 449px以下のサイズに適用
+//       settings: {
+//       slidesToShow: 1,
+//       },
+//     },
+//   ],
+// });
+});
+
+// });
